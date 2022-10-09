a22c814aef051db9f87b7aa745ba06c5e6799dba
TSchotter <troyswizard@gmail.com>
2022-10-09 16:26:28 -0400

    Added image and code to show it

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">AI Generated Images Of Me</header>
+            <img
+                src={require("./images/charcoal.png")}
+                alt="AI Generated charcoal image of me"
+            />
             <p>Hello World, Troy Schotter following tutorial for COS420</p>
         </div>
     );
