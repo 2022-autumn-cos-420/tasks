2d6f9959025045fb93ee85d4645d485bfc932c0d
TSchotter <troyswizard@gmail.com>
2022-10-09 16:33:58 -0400

    Added unorganized list and some list elements

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,16 @@ function App(): JSX.Element {
                 src={require("./images/charcoal.png")}
                 alt="AI Generated charcoal image of me"
             />
+            <div className="container">
+                <ul>
+                    <li>
+                        Take several pictures of myself with different
+                        backgrounds and different distances
+                    </li>
+                    <li>Convert images to a standardized size</li>
+                    <li>Feed images into DreamBooth Stable Diffusion</li>
+                </ul>
+            </div>
             <p>Hello World, Troy Schotter following tutorial for COS420</p>
         </div>
     );
