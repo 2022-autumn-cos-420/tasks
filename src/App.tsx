4de2660f77830f0a5f80e6fba4816e14912e2dcd
TSchotter <troyswizard@gmail.com>
2022-10-09 16:54:12 -0400

    Moved alot of elements around for two column format

--- src/App.tsx
+++ src/App.tsx
@@ -5,32 +5,39 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">AI Generated Images Of Me</header>
-            <img
-                src={require("./images/charcoal.png")}
-                alt="AI Generated charcoal image of me"
-            />
+            <header className="App-header">AI Generated Image Of Me</header>
             <div className="container">
-                <ul>
-                    <li>
-                        Take several pictures of myself with different
-                        backgrounds and different distances
-                    </li>
-                    <li>Convert images to a standardized size</li>
-                    <li>Feed images into DreamBooth Stable Diffusion</li>
-                </ul>
-                <div>
-                    <Button
-                        onClick={() => {
-                            alert("Jim");
-                            console.log("Hello World");
-                        }}
-                    >
-                        Click to say your name (prototype)
-                    </Button>
+                <div className="column">
+                    <img
+                        src={require("./images/charcoal.png")}
+                        alt="AI Generated charcoal image of me"
+                        className="Image"
+                    />
+                </div>
+                <div className="column">
+                    <ul>
+                        <li>
+                            Take several pictures of myself with different
+                            backgrounds and different distances
+                        </li>
+                        <li>Convert images to a standardized size</li>
+                        <li>Feed images into DreamBooth Stable Diffusion</li>
+                    </ul>
+                    <div>
+                        <Button
+                            onClick={() => {
+                                alert("Jim");
+                                console.log("Hello World");
+                            }}
+                        >
+                            Click to say your name (prototype)
+                        </Button>
+                    </div>
+                    <p>
+                        Hello World, Troy Schotter following tutorial for COS420
+                    </p>
                 </div>
             </div>
-            <p>Hello World, Troy Schotter following tutorial for COS420</p>
         </div>
     );
 }
