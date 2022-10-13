10526828c7489475c15c7910a3a502442e9e0c89
TSchotter <troyswizard@gmail.com>
2022-10-13 17:48:29 -0400

    Completed bookEndList and tripleNumbers

--- src/arrays.ts
+++ src/arrays.ts
@@ -5,7 +5,12 @@
  * the number twice.
  */
 export function bookEndList(numbers: number[]): number[] {
-    return numbers;
+    let newNums: number[] = [];
+    if (numbers.length > 0) {
+        newNums = [numbers[0], numbers[numbers.length - 1]];
+    }
+
+    return newNums;
 }
 
 /**
@@ -13,6 +18,7 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
+    numbers = numbers.map((numbers: number): number => numbers * 3);
     return numbers;
 }
 
