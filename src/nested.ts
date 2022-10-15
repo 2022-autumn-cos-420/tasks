3137fd610e6342b09b306317eb68790e72724be3
TSchotter <troyswizard@gmail.com>
2022-10-15 19:22:30 -0400

    finished removeQuestion

--- src/nested.ts
+++ src/nested.ts
@@ -48,7 +48,10 @@ export function findQuestion(
  * with the given `id`.
  */
 export function removeQuestion(questions: Question[], id: number): Question[] {
-    return [];
+    const newQuestions = questions.filter((obj: Question) => {
+        if (id != obj.id) return obj;
+    });
+    return newQuestions;
 }
 
 /***
