9ce2749547bce15e97330ad1030caaa64414f58d
TSchotter <troyswizard@gmail.com>
2022-10-15 19:31:24 -0400

    finished sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -81,7 +81,11 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    let sum: number = 0;
+    questions.forEach((obj: Question) => {
+        if (obj.published) sum += obj.points;
+    });
+    return sum;
 }
 
 /***
