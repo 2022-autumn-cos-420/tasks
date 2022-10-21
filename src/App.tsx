70e736e201f995ccd2e4d72ca60926f24e0d044b
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-21 17:29:12 -0400

    more HTML/CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -14,7 +15,13 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-
+            <div style={{ border: "1px solid blue", padding: "4px" }}>
+                border Activated.
+            </div>
+            <div>
+                This is <span style={{ color: "blue" }}>blue text</span>.
+            </div>
+            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
             <ol>
                 <li>First place</li>
                 <li>Second place</li>
