fb40516546f72b23972d5fda14c8166fa11cd87d
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-21 16:02:02 -0400

    Changed class name

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Modified by EDaran
+                UD COS420 with React Hooks and TypeScript Modified by EDaran
+                Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
