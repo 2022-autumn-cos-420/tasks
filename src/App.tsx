f2e7d9c43cc78c5bc621b57f1847c2ee955d79d9
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-21 16:11:48 -0400

    Testing basic HTML

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,21 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD COS420 with React Hooks and TypeScript Modified by EDaran
-                Hello World
+                <h1>
+                    UD COS420 with React Hooks and TypeScript Modified by EDaran
+                </h1>
+                <p>Hello World</p>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <ol>
+                <li>First place</li>
+                <li>Second place</li>
+                <li>Third place</li>
+            </ol>
         </div>
     );
 }
