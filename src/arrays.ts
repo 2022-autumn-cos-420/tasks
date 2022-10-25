94f6d00f11da83c0638351b892202b4f20d31ee1
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-25 17:00:55 -0400

    Completed another test

--- src/arrays.ts
+++ src/arrays.ts
@@ -74,7 +74,8 @@ export const shoutIfExclaiming = (messages: string[]): string[] => {
  * 4 letters long.
  */
 export function countShortWords(words: string[]): number {
-    return 0;
+    const fixedWords = words.filter((word: string): boolean => word.length < 4);
+    return fixedWords.length;
 }
 
 /**
