8772fa9b012fd017f77a667af64bc91702987774
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-25 12:44:40 -0400

    Added rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,44 @@
 import React from "react";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-<<<<<<< HEAD
                 <h1>
-                    UD COS420 with React Hooks and TypeScript Modified by EDaran
+                    UD COS420
+                    <Container>
+                        <Row>
+                            <Col>
+                                with React Hooks
+                                <div
+                                    id="rectangle"
+                                    style={{
+                                        height: "50px",
+                                        width: "70px",
+                                        backgroundColor: "red"
+                                    }}
+                                ></div>
+                            </Col>
+
+                            <Col>
+                                {" "}
+                                and TypeScript
+                                <div
+                                    id="rectangle"
+                                    style={{
+                                        height: "50px",
+                                        width: "70px",
+                                        backgroundColor: "red"
+                                    }}
+                                ></div>{" "}
+                            </Col>
+                        </Row>
+                    </Container>
+                    Modified by EDaran
                 </h1>
                 <p>Hello World</p>
-=======
-                UM COS420 with React Hooks and TypeScript
->>>>>>> upstream/task-html-css
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
@@ -25,7 +50,15 @@ function App(): JSX.Element {
             <div>
                 This is <span style={{ color: "blue" }}>blue text</span>.
             </div>
-            <Button onClick={() => console.log("I am logged")}>Click Me</Button>
+            <div>
+                <img
+                    src="https://umaine.edu/spa/wp-content/uploads/sites/32/2022/09/The-Melancholy-Play-Poster-V1.1.png"
+                    alt="Only a few days left to see the play!"
+                />
+            </div>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <ol>
                 <li>First place</li>
                 <li>Second place</li>
