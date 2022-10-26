3d929eab68c2e09077d93819a17ceecc95a0f644
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-26 17:23:35 -0400

    Completed isCorrect Test

--- src/objects.ts
+++ src/objects.ts
@@ -26,6 +26,9 @@ export function makeBlankQuestion(
  * HINT: Look up the `trim` and `toLowerCase` functions.
  */
 export function isCorrect(question: Question, answer: string): boolean {
+    if (question.expected.toLowerCase() === answer.trim().toLowerCase()) {
+        return true;
+    }
     return false;
 }
 
