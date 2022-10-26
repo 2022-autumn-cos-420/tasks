a482a48eff19018d6ca82a448d3ccb3566f56852
Elijah D'Aran <elijah.daran@gmail.com>
2022-10-25 21:32:12 -0400

    Completed Question Test

--- src/functions.ts
+++ src/functions.ts
@@ -13,7 +13,18 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let sum = 0;
+    if (first > 0) {
+        sum += first;
+    }
+    if (second > 0) {
+        sum += second;
+    }
+    if (third > 0) {
+        sum += third;
+    }
+
+    return sum;
 }
 
 /**
@@ -21,7 +32,9 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    let newmessage = message.toUpperCase();
+    newmessage += "!";
+    return newmessage;
 }
 
 /**
@@ -29,7 +42,9 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    const truthValue = message.slice(-1) === "?" ? true : false;
+
+    return truthValue;
 }
 
 /**
