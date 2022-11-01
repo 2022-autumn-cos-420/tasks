fc33934aba86a67e074fd0b0d1d78da66126e2e4
Elijah D'Aran <elijah.daran@gmail.com>
2022-11-01 11:06:38 -0400

    sameType Test

--- src/nested.ts
+++ src/nested.ts
@@ -168,7 +168,16 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length !== 0) {
+        const FirstQuestion = questions[0];
+        const firstType = FirstQuestion.type;
+        const allSameType = questions.every(
+            (question: Question): boolean => question.type === firstType
+        );
+        return allSameType;
+    } else {
+        return true;
+    }
 }
 
 /***
