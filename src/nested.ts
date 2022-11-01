79e9ebe84b5ed0d3b2312a8a48751442957d3067
Elijah D'Aran <elijah.daran@gmail.com>
2022-11-01 06:43:50 -0400

    toCSV Test

--- src/nested.ts
+++ src/nested.ts
@@ -116,7 +116,23 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const questionCSV = questions
+        .map(
+            (question: Question): string =>
+                question.id +
+                "," +
+                question.name +
+                "," +
+                question.options.length +
+                "," +
+                question.points +
+                "," +
+                question.published
+        )
+        .join("\n");
+
+    const finalString = "id,name,options,points,published" + "\n" + questionCSV;
+    return finalString;
 }
 
 /**
