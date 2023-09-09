3c7555313e2939cc77b30783e051538da4b49f4b
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-09-09 17:23:48 -0400

    Add image column and button to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,52 @@
 import React from "react";
 import "./App.css";
-
+import img from "./images/image.jpg";
+import { Button, Col, Container, Row } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
+            <header
+                className="App-header"
+                style={{ backgroundColor: "yellowgreen" }}
+            >
                 UD COS420 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
-            <p> Hello World my Name is Kaleb Hannan</p>
+            <h1> Hello World my Name is Kaleb Hannan</h1>
+            <img src={img} alt="A picture of a road" height={200} width={200} />
+            <ul>
+                <li>apples</li>
+                <li>oranges</li>
+                <li>bananas</li>
+            </ul>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                height: "50px",
+                                width: "400px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                height: "50px",
+                                width: "400px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 }
