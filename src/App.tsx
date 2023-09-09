d9eb9e39ef493385b32af242b82569c7ea5ac800
Kaleb Hannan <kaleb.hannan@maine.edu>
2023-09-09 14:18:41 -0400

    try to fix merge

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD cos with React Hooks and TypeScript
+                UD COS 420 with React Hooks and TypeScript
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
